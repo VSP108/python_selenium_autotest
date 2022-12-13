ee91f16223349db38f1279a27465098c12658a20
VSP108 <ponomarev.vsp@gmail.com>
2022-12-13 14:19:31 +0800

    move to pets home

--- example1.py
+++ example1.py
@@ -7,8 +7,8 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 driver = webdriver.Chrome("C:/Users/CryptoVed/Desktop/IT/Projects_Skillfactory/python_selenium_autotest>chromedriver")
-driver.get("https://google.com")
-driver.find_element(By.XPATH, "//input[@title=\"Поиск\"]").send_keys('Skillfactory' + Keys.RETURN)
-sleep(2)
-driver.save_screenshot('sf.png')
+driver.get("http://130.193.37.179/app/pets")
+(driver.find_elements(By.XPATH, "//*[@id=\"image\"]/img"))[1].click()
+sleep(3)
+driver.save_screenshot('pet_home.png')
 driver.quit()
